be653d21d2aa772bb845be9446f463e9f2530d73
Scharmy <scharmy.pompy@gmail.com>
2019-07-16 00:31:24 +0000

    Add a Form to the Individual Place Page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @google_api_key = Figaro.load['GEOCODER_API_KEY']
+    @comment = Comment.new
   end
 
   def edit

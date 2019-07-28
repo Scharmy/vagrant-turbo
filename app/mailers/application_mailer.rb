1b83be31f808542c77007ae4410e63e8dc043824
Scharmy <scharmy.pompy@gmail.com>
2019-07-28 17:14:57 +0000

    Completed setting up Text and HTML for the Action Mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@nomsterapp.com"
-
-  def comment_added
-    mail(to: "scharmy.pompy@gmail.com",
-          subject: "A comment has been added to your place")
-  end
+  default from: 'from@example.com'
+  layout 'mailer'
 end
+

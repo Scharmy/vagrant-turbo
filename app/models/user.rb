6ecba948a5de57cbc3543189d47fbcdfad3e2e4d
Scharmy <scharmy.pompy@gmail.com>
2019-07-31 15:42:56 +0000

    updating the User.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Removed trackable from the devise
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :places
   has_many :comments
